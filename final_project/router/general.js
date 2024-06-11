74ebe2302fc2b37410d13a137f6c2512726c7bd1
AmazianeSana <amaziane.sa@gmail.com>
2024-06-11 15:48:47 -0400

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -10,22 +10,29 @@ public_users.post("/register", (req,res) => {
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  if (books.length === 0) {
+// Get the list of books available in the shop
+public_users.get('/', (req, res) => {
+  if (Object.keys(books).length === 0) {
     return res.status(404).json({ message: "No books available" });
   }
-  return res.status(200).json(books);
-
+  // Use JSON.stringify to format the output neatly
+  const formattedBooks = JSON.stringify(books, null, 2); // 2 spaces for indentation
+  return res.status(200).send(formattedBooks);
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
-  
+public_users.get('/isbn/:isbn', (req, res) => {
+  const isbn = req.params.isbn;
+
+  // Assuming ISBN is the key in the books object
+  const book = books[isbn];
+
+  if (!book) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+
+  return res.status(200).json(book);
+});
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
